9941b8c31336a7606c5a1497d13dd377076d0f84
Ngoguey42 <ngoguey@student.42.fr>
2015-04-09 16:30:34 +0200

    updated ex03

--- j03_Inheritance/ex03/ClapTrap.cpp
+++ j03_Inheritance/ex03/ClapTrap.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/04/09 12:38:12 by ngoguey           #+#    #+#             //
-//   Updated: 2015/04/09 12:40:36 by ngoguey          ###   ########.fr       //
+//   Updated: 2015/04/09 16:30:10 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -19,7 +19,7 @@
 
 ClapTrap::ClapTrap() :
 _name("Unknown"), _hp(100), _hpMax(100), _mana(100), _manaMax(100), _level(1),
-_meleeDamage(30), _rangedDamage(20), _armorReduction(5), _class("CL4P")
+_meleeDamage(30), _rangedDamage(20), _armorReduction(5)
 {
 	std::cout << "ClapTrap() constructor called!" << std::endl;
 	return ;
@@ -27,15 +27,17 @@ _meleeDamage(30), _rangedDamage(20), _armorReduction(5), _class("CL4P")
 
 ClapTrap::ClapTrap(std::string const &name) :
 _name(name), _hp(100), _hpMax(100), _mana(100), _manaMax(100), _level(1),
-_meleeDamage(30), _rangedDamage(20), _armorReduction(5), _class("CL4P")
+_meleeDamage(30), _rangedDamage(20), _armorReduction(5)
 {
 	std::cout << "ClapTrap(name) constructor called!" << std::endl;
 	return ;
 }
 
 ClapTrap::ClapTrap(ClapTrap const & src) :
-_name(src._name), _hp(100), _hpMax(100), _mana(100), _manaMax(100), _level(1),
-_meleeDamage(30), _rangedDamage(20), _armorReduction(5), _class("CL4P")
+	_name(src._name), _hp(src._hp), _hpMax(src._hpMax),
+	_mana(src._mana), _manaMax(src._manaMax), _level(src._level),
+	_meleeDamage(src._meleeDamage), _rangedDamage(src._rangedDamage),
+	_armorReduction(src._armorReduction)
 {
 	std::cout << "ClapTrap(src) constructor called!" << std::endl;
 	return ;
