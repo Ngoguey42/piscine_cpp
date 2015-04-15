65ab29a22d1be17ae6dcc262303f7bd9a40680d4
Ngoguey42 <ngoguey@student.42.fr>
2015-04-15 10:00:26 +0200

    improved tests

--- j07_Templates/ex00/whatever.cpp
+++ j07_Templates/ex00/whatever.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/03/30 07:46:00 by ngoguey           #+#    #+#             //
-//   Updated: 2015/03/30 07:52:03 by ngoguey          ###   ########.fr       //
+//   Updated: 2015/04/15 09:34:13 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -38,12 +38,17 @@ int			main(void)
 {
 	int a = 2;
 	int b = 3;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "swap" << std::endl;
 	::swap( a, b );
+	
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "swap" << std::endl;
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
